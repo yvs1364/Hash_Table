4aed1a304f04c40d54a8b0630deeda827c8d3e1c
yvs1364 <yvs.pro64@gmail.com>
2022-07-06 17:27:34 +0200

    modif
    ajout list

--- main.c
+++ main.c
@@ -1,36 +1,116 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct Student Student;
+typedef struct List List;
+
+#define PRESENT(list, student)({        \
+ if(list == NULL || student == NULL){   \
+        exit(EXIT_FAILURE);             \
+    }                                   \
+})                                      \
+
 struct Student{
     char *name;
+    int age;
+    Student *next;
 };
 
-int hachage(char *chaine){
-    int i = 0, nombreHache = 0;
+struct List{
+    Student *first;
+};
 
-    for (i = 0 ; chaine[i] != '\0' ; i++){
-        nombreHache += chaine[i];
-    }
-    nombreHache %= 100;
 
-    return nombreHache;
-}
-/******************************************************************************
-// Grâce à cette fonction de hachage, vous savez donc dans quelle case de votre tableau vous devez placer vos données ! Lorsque vous voudrez y accéder plus tard pour en récupérer les données, il suffira de hacher à nouveau le nom de la personne pour retrouver l'indice de la case du tableau où sont stockées les informations !
+List *init();
+int sizeOfList(List *list);
+int hash(char *list);
+void insert(List *list, char* newName, int newAge);
+void showList(List *list);
 
-//Je vous recommande de créer une fonction de recherche qui se chargera de hacher la clé (le nom) et de vous renvoyer un pointeur vers les données que vous recherchiez.
 
-//Cela donnerait par exemple :
-//infosSurLuc = rechercheTableHachage(tableau, "Luc Doncieux");
-*******************************************************************************/
-int main(){
-    Student student1, student2;
-    student1.name = "Luc Doncieux";
-    student2.name = "Julien Lefebvre";
 
-    printf("Value du nom : %i\n", hachage(student1.name));
-    printf("Value du nom : %i\n", hachage(student2.name));
+int main() {
+    List *myList = init();
+
+
+    printf("------------------------------------\n");
+    showList(myList);
+    printf("------------------------------------\n");
+    insert(myList, "Luc Doncieux", 25);
+    showList(myList);
+    printf("------------------------------------\n");
+    sizeOfList(myList);
+    printf("------------------------------------\n");
+
 
     return 0;
+}
+
+List *init() {
+    // Initiliase la liste chainee
+    List *list = malloc(sizeof(*list));
+    Student *student = malloc(sizeof(*student));
+
+    PRESENT(list,student);
+    student->name = "Jean-eude";
+    student->age  = 26;
+    student->next = NULL;
+    list->first   = student;
+
+    return list;
+}
+
+void insert(List *list, char* newName, int newAge){
+    Student *new = malloc(sizeof(*new));
+    PRESENT(list,new);
+    new->name = newName;
+    new->age = newAge;
+    new->next = list->first;
+    list->first = new;
+
+}
+
+
+void showList(List *list) {
+    if(list == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    Student *current = list->first;
+
+    while (current != NULL){
+        printf("[ Nom -> %s, ", current->name);
+        printf("age -> %d ", current->age);
+        printf("{ Hash du nom = %i }]", hash(current->name));
+        current = current->next;
+        printf(" -> ");
+    }
+    printf(" [ NULL ]\n");
+
+}
+
+int sizeOfList(List *list) {
+    int sizeList = 0;
+    if(list == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    Student *current = list->first;
+    while (current != NULL){
+       ++sizeList;
+        current = current->next;
+    }
+
+    return sizeList;
+}
+
+int hash(char *list){
+  int numbHash = 0;
+
+    for (int i=0; list[i] != '\0'; i++){
+        numbHash += list[i];
+    }
+    numbHash %= 100;
+
+    return numbHash;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+void insert(List *list, int newdata) {
+//     Student *new = malloc(sizeof(*new));
+//     PRESENT(list,new);
+//     new->data = newdata;        // Pointe le nouvel Element vers son futur successeur
+//     new->next = list->first;    // Fait pointer le prochain Element vers le first Element
+//     list->first = new;          // Fait pointer le point first vers le nouvel Element
+
+// }
